7876a1aa9c77d286fe6d496f4972c1357f5a5c65
Andrew Snake <andrew0000snake@gmail.com>
2014-08-29 22:42:19 +0700

    added ldapsrc script and its  result

---
+++ ldapsrc_rosbt.local_tanin.a.a.sh
@@ -0,0 +1,47 @@
+#! /bin/tcsh
+echo "" >cn_b;
+echo "">cns_dirty;
+echo "">cns_clean;
+
+#ou=users,dc=main_office,dc=rosbt,dc=com
+set ldapurl="ldap://ldap.rosbt.com:389"
+set basedn="OU=users,dc=main_office,DC=rosbt,DC=com"
+set binddn="uid=tanin.a.a,ou=users,dc=main_office,dc=rosbt,dc=com"
+set ldappas="rdc831\!#"
+
+##ldapsrc="ldapsearch -LLL -H $ldapurl -b $basedn -D $binddn -w $ldappas"
+##ldapsearch -LLL -H $ldapurl -b $basedn -D $binddn -w $ldappas cn mail
+##$ldapsrc | awk -F 'cn::' '{print $2}'> cn_b;
+
+set ldapsrc="ldapsearch -LLL -H $ldapurl -b $basedn -D $binddn -w $ldappas cn mail "
+#$ldapsrc >tmp1;
+$ldapsrc;
+#cat tmp1| awk -F 'cn::' '{print $2}'> cn_b;
+#cat tmp1| awk -F 'dn:' '{print $0}'> mail_b;
+
+
+#$ldapsrc > cn_b;
+#cat cn_b|head -n 15;
+#cat mail_b|head -n 15
+
+
+##foreach i ( `$ldapsrc | awk -F 'cn::' '{print $2}'` )
+#foreach i ( `cat cn_b` )
+#    echo $i|base64 -d>> cns_dirty;
+#    echo "" >> cns_dirty;
+#end
+#cat cns_dirty|grep -v "incomplete"> cns_clean;
+
+#foreach n ( `cat mail_b|| awk -F 'dn: uid' '{print $0}'`)
+#    cat mail_b|awk -F 'dn:' ' {print $0}'>>tmp2;
+#    cat mail_b|awk -F 'dn:' 'NR=='$n' {print }';
+#    echo $n;
+#    set t1=echo $n;
+    echo ------------ #>> tmp2;
+#end;
+
+
+#echo ldapurl=$ldapurl;
+#echo basedn=$basedn;
+#echo binddn=$binddn;
+#echo ldapsrc=$ldapsrc;
